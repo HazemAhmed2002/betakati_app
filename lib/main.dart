4e58b7ea3438093924ec71c6088decb37d96e243
HazemAhmed2002 <ahmedhazeml734@gmail.com>
2023-03-24 14:52:07 +0200

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.cyan[700],
-        body: Container(),
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(
+              child: Text("Container 1"),
+              color: Colors.red,
+              height: 100.0,
+              width: 100.0,
+            ),
+            Container(
+              child: Text("Container 2"),
+              color: Colors.green,
+              height: 100.0,
+              width: 100.0,
+            ),
+            Container(
+              child: Text("Container 3"),
+              color: Colors.blue,
+              height: 100.0,
+              width: 100.0,
+            ),
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
